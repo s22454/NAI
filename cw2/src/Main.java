d3af9548ce26d212dfdf5396a27ef8e68694b8ee
Mateusz Sasor-Adamczyk <s22454@pjwstk.edu.pl>
2022-03-28 11:43:16 +0200

    usunalem stare czytanie z pliku, dodalem input od uzytkownika, zmienilem konstruktor perceptronu

--- cw2/src/Main.java
+++ cw2/src/Main.java
@@ -1,12 +1,36 @@
 import java.io.IOException;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        try (Scanner scanner = new Scanner(System.in)){
 
-        try {
-            Perceptron p = new Perceptron("data_for_perceptron/iris_perceptron/test.txt");
+            //init data
+            Double learningConstant;
+            System.out.print("Podaj stala uczenia sie: ");
+            learningConstant = Double.parseDouble(scanner.nextLine());
+
+            Double threshold;
+            System.out.print("\nPodaj prog aktywacji: ");
+            threshold = Double.parseDouble(scanner.nextLine());
+
+            //learning data
+            String trainingPath;
+            System.out.print("\nPodaj sciezke do pliku z danymi treningowymi: ");
+            trainingPath = scanner.nextLine();
+
+            double accuracy;
+            System.out.print("\nPodaj docelowa dokladnosc: ");
+            accuracy = Double.parseDouble(scanner.nextLine());
+
+            //creating and training perceptron
+            Perceptron p = new Perceptron(learningConstant, threshold);
+            
         } catch (IOException e) {
             e.printStackTrace();
         }
+
+
+        //"data_for_perceptron/iris_perceptron/test.txt"
     }
 }
--- cw2/src/Perceptron.java
+++ cw2/src/Perceptron.java
@@ -5,23 +5,16 @@ import java.util.HashMap;
 import java.util.Scanner;
 
 public class Perceptron {
-    HashMap<ArrayList<Double>,String> data;
 
-    public Perceptron(String path) throws IOException {
-        data = new HashMap<>();
-        Scanner scanner = new Scanner(new File(path));
+    private HashMap<ArrayList<Double>,String> data;
+    private double learningConstant;
+    private double threshold;
 
-        while(scanner.hasNext()){
-            String[] cells = scanner.next().split(",");
-            ArrayList<Double> tmpList = new ArrayList<>();
-
-            for (int i = 0; i < cells.length - 1; i++)
-                tmpList.add(Double.parseDouble(cells[i]));
-
-            data.put(tmpList, cells[cells.length - 1]);
-        }
-
-        for (String s : data.values())
-            System.out.println(s);
+    public Perceptron(double learningConstant, double threshold) throws IOException {
+        this.learningConstant   = learningConstant;
+        this.threshold          = threshold;
+        data                    = new HashMap<>();
     }
+
+
 }
