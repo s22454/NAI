92ecc80afaf0113f8e3d68115205820d1903d6a5
saoras01 <skrzatekcom1@gmail.com>
2022-04-05 16:45:26 +0200

    zmienilem interfejs komunikacyjny z u zytkownikiem tak aby ulatwic sobie testy

--- cw3/src/Main.java
+++ cw3/src/Main.java
@@ -8,15 +8,30 @@ public class Main {
         System.out.println("---------------------------------------------");
 
         Network network = new Network(new File("./train"));
+        System.out.println("KONIEC UCZENIA");
 
         try (Scanner scanner = new Scanner(System.in)) {
-            System.out.print("Podaj sciezke do pliku testowego: ");
-            String testPath = scanner.nextLine();
+            String input = "";
+            while (!input.equals("n")) {
+                System.out.println("---------------------------------------------");
+                System.out.print("Chcesz podac plik testowy? (y - tak; n - nie) Odp: ");
+                input = scanner.nextLine();
+
+                if (input.equals("y")) {
+                    System.out.print("Podaj sciezke do pliku testowego: ");
+                    String testPath = scanner.nextLine();
+
+                    System.out.println("---------------------------------------------");
+                    System.out.println("Podany plik jest w jezyku: " + network.test(testPath));
+                }
+            }
 
             System.out.println("---------------------------------------------");
-            System.out.println("Podany plik jest w jezyku: " + network.test(testPath));
+            System.out.println("KONIEC TESTOWANIA PLIKOW");
+            System.out.println("---------------------------------------------");
+            System.out.println("TESTY UZYTKOWNIKA");
 
-            String input = "";
+            input = "";
             while (!input.equals("n")){
                 System.out.println("---------------------------------------------");
                 System.out.print("Chcesz podac wlasny tekst? (y - tak; n - nie) Odp: ");
